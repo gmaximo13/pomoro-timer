d2a57f772746ace1dea790b68ae832dab86c5c3b
Gustavo Máximo <gustavo.maximo80@gmail.com>
2026-08-16 19:08:46 -0300

    feat: add actions buttons from home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 import { TScreenDefinitionsProps } from "../AppRoutes";
 import { Theme } from "../shared/themes/Theme";
@@ -9,11 +9,67 @@ export const Home = () => {
 
   return (
     <View>
-      <Text style={{ fontFamily: 'InterRegular', color: Theme.colors.text }}>Home Page</Text>
-
-        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-            <Text style={{ fontFamily: 'InterRegular', color: Theme.colors.text }} >Settings</Text>
+      <View style={ styles.buttonContainer }>
+        <TouchableOpacity style={ styles.primaryButton }>
+            <Text style={ styles.primaryButtonText }>Iniciar</Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={ styles.buttonContainer }>
+        <TouchableOpacity style={ styles.primaryButton }>
+            <Text style={ styles.primaryButtonText }>Pausar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={ styles.secondaryButton }>
+            <Text style={ styles.secondaryButtonText }>Parar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={ styles.buttonContainer }>
+        <TouchableOpacity style={ styles.primaryButton }>
+            <Text style={ styles.primaryButtonText }>Continuar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={ styles.secondaryButton }>
+            <Text style={ styles.secondaryButtonText }>Reiniciar</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  primaryButton: {
+    backgroundColor: Theme.colors.primary,
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    borderRadius: 55,
+  },
+
+  primaryButtonText: {
+    fontFamily: 'InterRegular',
+    fontSize: Theme.fontSizes.medium,
+    color: Theme.colors.text,
+  },
+
+  secondaryButton: {
+    borderWidth: 2,
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    borderRadius: 55,
+    borderColor: Theme.colors.primary,
+  },
+
+  secondaryButtonText: {
+    fontFamily: 'InterRegular',
+    fontSize: Theme.fontSizes.medium,
+    color: Theme.colors.text,
+  },
+
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 16,
+  },
+});
